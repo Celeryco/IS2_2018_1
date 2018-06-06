7c98bdd9daf9887fe59002acfc82ae0cddb06823
Eros Campos <eroscamposg@gmail.com>
2018-06-06 02:30:19 -0500

    mapa prueba hecho

--- test-eros/templates/template_1.py
+++ test-eros/templates/template_1.py
@@ -1,41 +1,34 @@
 ## Pygame template - skeleton for a new python project
 import pygame
 import random
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-## Useful colors
-WHITE = (255 ,255 ,255)
-BLACK = (0 ,0 ,0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0 ,0 ,255)
+import settings
+from settings import *
 
 ## Initialize pygame and create window
 pygame.init()
 # Initialize Sounds
 pygame.mixer.init()
 
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
-pygame.display.set_caption("My Game")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
 
 ## Game Loop
 running = True
 while running:
-    clock.running(FPS)
+    clock.tick(FPS)
     # Process input(events)
-    if event in pygame.event.get():
+    for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
 
     # Update
-
+    all_sprites.update()
     # Draw / render
     screen.fill(BLACK)
+    all_sprites.draw(screen)
     # to avoid lag. After finishing drawing
     pygame.display.flip()
 
