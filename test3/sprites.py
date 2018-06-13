e867d17671ec87a795203d11d57f9d9a5400a3cd
Angelo <angcastillo18@gmail.com>
2018-06-13 03:24:11 -0500

    carpeta test3 , jugador en mapa

---
+++ test3/sprites.py
@@ -0,0 +1,104 @@
+import pygame as pg
+from settings import *
+vec=pg.math.Vector2
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.game.player_img.set_clip(pg.Rect(0, 0, 52, 76))
+
+        self.image = self.game.player_img.subsurface(self.game.player_img.get_clip())
+        self.rect = self.image.get_rect()
+        ##########33###
+        self.frame = 0
+        self.left_states = { 0: (0, 76, 52, 76), 1: (52, 76, 52, 76), 2: (156, 76, 52, 76) }
+        self.right_states = { 0: (0, 152, 52, 76), 1: (52, 152, 52, 76), 2: (156, 152, 52, 76) }
+        self.up_states = { 0: (0, 228, 52, 76), 1: (52, 228, 52, 76), 2: (156, 228, 52, 76) }
+        self.down_states = { 0: (0, 0, 52, 76), 1: (52, 0, 52, 76), 2: (156, 0, 52, 76) }
+        #########
+        self.vel=vec(0,0)
+        self.pos=vec(x,y) * TILESIZE
+###########################
+    def get_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > (len(frame_set) - 1):
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clipped_rect):
+        if type(clipped_rect) is dict:
+            self.game.player_img.set_clip(pg.Rect(self.get_frame(clipped_rect)))
+        else:
+            self.game.player_img.set_clip(pg.Rect(clipped_rect))
+        return clipped_rect
+
+#############
+    def get_keys(self):
+        self.vel=vec(0,0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.clip(self.left_states)
+            #self.clip(self.left_states[0])
+            self.vel.x = -PLAYER_SPEED
+            self.image = self.game.player_img.subsurface(self.game.player_img.get_clip())
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            #self.clip(self.right_states[0])
+            self.clip(self.right_states)
+            self.vel.x = PLAYER_SPEED
+            self.image = self.game.player_img.subsurface(self.game.player_img.get_clip())
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.clip(self.up_states)
+            #self.clip(self.up_states[0])
+            self.vel.y = -PLAYER_SPEED
+            self.image = self.game.player_img.subsurface(self.game.player_img.get_clip())
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.clip(self.down_states)
+            #self.clip(self.down_states[0])
+            self.vel.y = PLAYER_SPEED
+            self.image = self.game.player_img.subsurface(self.game.player_img.get_clip())
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel.x *= 0.7071
+            self.vel.y *= 0.7071
+
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
+
+    def update(self):
+        self.get_keys()
+        self.pos+= self.vel * self.game.dt
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
